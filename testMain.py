d779708edbd11865f9b356221651de9d8c528023
Shay Chen <shaychen@can-dao.com>
2019-08-28 16:05:41 +0800

    修改，里面有报错，回家解决

--- testMain.py
+++ testMain.py
@@ -1,5 +1,6 @@
 #!/bin/bash/python
 
+import mysql.connector
 from PyQt5.QtWidgets import QMainWindow
 from mainWindow import *
 
@@ -7,11 +8,35 @@ from mainWindow import *
 class MainWindowEdit(QMainWindow, Ui_MainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-        Ui_MainWindow.setupUi(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        sql = self.text_Input.toPlainText()
+        self.btn_queary.clicked(queary)
+        # self.text_message_print.setText(result)
+
+
+def queary(sql):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="123456",
+        database="test"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute(sql)
+    result = mycursor.fetchall()
+    headList = mycursor.description
+    for index in range(len(headList)):
+        if index == 0:
+            heading = [headList[index][0], ]
+        else:
+            heading.append(headList[index][0])
+    return heading, result
 
 
 if __name__ == '__main__':
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindowEdit()
     window.show()
